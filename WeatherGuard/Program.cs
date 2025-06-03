ed53375e0fb0276aed3c12e59e41f838f7f19dba
Luigi <159388072+LuigiBerzaghi@users.noreply.github.com>
2025-06-03 17:33:47 -0300

    Update Program.cs

--- WeatherGuard/Program.cs
+++ WeatherGuard/Program.cs
@@ -3,16 +3,23 @@ using Microsoft.EntityFrameworkCore;
 using WeatherGuard.Data;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=weatherguard.db"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
